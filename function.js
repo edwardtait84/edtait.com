949dcc5965e13ef692551404af60de1b850b32dc
EYP Websites (Edward Tait) <139652245+edwardtait84@users.noreply.github.com>
2024-05-03 13:36:24 +0100

    Update function.js

--- function.js
+++ function.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     { text: "<i>Engaging from the outset</i>", author: "Oscar Maclean, performer of his <i>String Trio</i>" },
     { text: "<i>Excellent technique, knowledge of repertoire and is a pleasure to teach</i>", author: "Jacques Cohen" },
     { text: "<i>His works show a nostalgic feel, yet also a unique and individual stamp</i>", author: "Rhys Stokes, performer of <i>Mo Li Hua</i> and <i>Fleeting Manhattan</i>" },
+    { text: "<i>Wonderful touches and a very satisfying shape</i>", author: "Patrick Bailey, on his <i>Rhapsody for Sextet</i>" },
     { text: "<i>Undoubtedly a very exciting upcoming compositional voice</i>", author: "Samuel Kemp" },
     { text: "<i>A vivid and imaginative work, well-written for cello</i>", author: "Timothy Bowers" },
     { text: "<i>Marrying movement and zeitgeist, Fleeting Manhattan masterly paints a panoramic fresco of a thriving nation</i>", author: "Dexter Dédalo" },
